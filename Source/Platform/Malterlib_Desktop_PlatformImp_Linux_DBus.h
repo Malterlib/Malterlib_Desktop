12447dfd0a203a13a8b5d253f29870cf7f03876b
Erik Olofsson <erik.olofsson@hansoft.com>
2016-01-02 09:39:40 +0000

    [Malterlib] Update license
    [grafted history before @55251]
    
    Copied from Perforce
     Change: 55250

---
+++ Source/Platform/Malterlib_Desktop_PlatformImp_Linux_DBus.h
@@ -0,0 +1,457 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+/*
+	File:			DBus utility interface
+
+	Author:			Michael Wynne
+
+	Contents:		NDBus:
+						CMessage
+						CMessageReader
+						CMessageWriter
+						CError
+						CConnection
+						CSystem
+
+						CDBusException
+						DMibDBusError
+
+	Comments:		You should know how DBus works before using this:	
+						https://en.wikipedia.org/wiki/D-Bus
+
+					You should use the spec & api ref as reference when fixing this:
+						Spec: http://dbus.freedesktop.org/doc/dbus-specification.html
+						libdbus: http://dbus.freedesktop.org/doc/api/html/
+
+					The following C++ types are mapped to dbus argument types:
+						uint8 				DBUS_TYPE_BYTE
+						int16				DBUS_TYPE_INT16
+						uint16 				DBUS_TYPE_UINT16
+						int32				DBUS_TYPE_INT32
+						uint32 				DBUS_TYPE_UINT32
+						int64				DBUS_TYPE_INT64
+						uint64 				DBUS_TYPE_UINT64
+						bool 				DBUS_TYPE_BOOLEAN
+						CStr 				DBUS_TYPE_STRING
+						TCVector<uint8>		DBUS_TYPE_ARRAY of DBUS_TYPE_BYTE
+						TCVector<CStr>		DBUS_TYPE_ARRAY of DBUS_TYPE_STRING
+
+					The DBus exception CDBusException is only thrown when some bad stuff does down.
+
+					TODO: The other types, including variant.
+					TODO: A proper message loop. Only blocking methods supported atm.
+					TODO: Registering to receive requests.
+
+	Usage:
+
+			Also see Test_DBus.cpp for working example code.
+	
+			// Sub system setup
+			NDBus::CSystem DBus;
+
+			if (!DBus.f_IsOK())	
+				return; // Could not load dbus dynamic lib.
+
+			// Message creation
+			NDBus::CMessage Msg(	NDBus::EMessageType_Method
+								, 	"com.malterlib.service"
+								, 	"/modules/test"
+								,	"com.hansoft.test"
+								,	"testMethod"
+								,	DBus);
+
+			// Writing a message
+			{
+				NDBus::CMessageWriter Writer(Msg);		
+
+				TCVector<uint8> lBytes;
+				...
+
+				if (!Writer.f_AppendArgs( (uint32)1, CStr("Str"),  false, lBytes))
+				{
+					// Error
+				}
+			}
+
+			NDBus::CMessage Reply;
+
+			NDBus::CConnection Conn(DBus);
+			if (!Conn.f_Open("com.malterlib.target"))
+			{
+				// Error
+				// Use Conn.f_GetLastError();
+			}
+
+			if (!Conn.f_BlockingSendWithReply(Msg, Reply, gc_DefaultTimeout)) 
+			{
+				// Error
+				// Use Conn.f_GetLastError();
+			}
+
+			// Reading a message
+			{
+				NDBus::CMessageReader Reader(Reply);		
+				
+				// Assuming we get back what we sent out.
+				uint32 UInt32Var;
+				CStr StringVar;
+				bool bBoolVar;
+				TCVector<uint8> lBytesVar;
+
+				if (!Reader.f_PopArgs( UInt32Var, StringVar, bBoolVar, lBytesVar))
+				{
+					// Error
+				}
+			}
+*/
+#pragma once
+
+#include <Mib/Core/Core>
+#include <Mib/Core/DynamicLibrary>
+
+extern "C"
+{
+	#include "dbus/dbus.h" // TODO: Hide completely?
+}
+
+#define DMibDBusError(_Msg) DMibImpError(CDBusException, _Msg);
+
+namespace NMib
+{
+
+	namespace NDBus
+	{
+
+		DMibImpErrorClass(CDBusException, NException::CException);
+
+		struct CDBusLibrary;
+
+		enum EMessageType
+		{
+				EMessageType_Unknown
+			,	EMessageType_Method
+			,	EMessageType_MethodReturn
+			,	EMessageType_Signal
+			,	EMessageType_Error
+		};
+
+		class CMessageWriter;
+		class CMessageReader;
+		class CError;
+		class CConnection;
+		class CSystem;
+
+		class CMessage
+		{
+		private:
+			CDBusLibrary& mp_Lib;
+			DBusMessage* mp_pMsg;
+
+			friend CMessageReader;
+			friend CMessageWriter;
+			friend CConnection;
+
+			CMessage(DBusMessage* _pMsg, CSystem& _System);
+			CMessage(DBusMessage* _pMsg, CDBusLibrary& _Lib);
+
+		public:
+			// Only to be used for replies.
+			CMessage(CSystem& _System);
+
+			// TODO: Move these into static methods?
+
+			// New method call message. _Type MUST == EMessageType_Method
+			CMessage(		EMessageType _Type
+						,	char const* _pService
+						,	char const* _pPath
+						,	char const* _pInterface
+						,	char const* _pMethod
+						, 	CSystem& _System);		
+
+			// New method return message. _Type MUST == EMessageType_MethodReturn
+			CMessage(		EMessageType _Type
+						,	CMessage& _InReplyTo
+						, 	CSystem& _System);		
+
+			// New signal message. _Type MUST == EMessageType_Signal
+			CMessage(		EMessageType _Type
+						,	char const* _pPath
+						,	char const* _pInterface
+						,	char const* _pName
+						, 	CSystem& _System);		
+
+			// New error message. _Type MUST == EMessageType_Error
+			CMessage(		EMessageType _Type
+						,	CMessage& _InReplyTo
+						,	char const* _pErrorName
+						, 	char const* _pErrorMessage
+						, 	CSystem& _System);		
+
+			CMessage(CMessage const& _ToCopy);
+			CMessage(CMessage&& _ToMove);
+			~CMessage();
+
+			CMessage& operator=(CMessage& _ToCopy);
+			CMessage& operator=(CMessage&& _ToMove);
+
+			EMessageType f_GetType() const;
+
+			void f_SetSerial(uint32 _Serial); // Only used for testing, to pretend to have been sent.
+
+		};
+
+		class CMessageWriter
+		{
+		private:
+			CDBusLibrary& mp_Lib;
+			DBusMessageIter mp_Iter;
+
+		public:
+			CMessageWriter(CMessage& _Message);
+			~CMessageWriter();
+
+			template<typename t_CType>
+			bool f_AppendArg(t_CType const& _Arg);
+
+			inline bool f_AppendArgs();
+
+			template<typename t_CFirst, typename... Args>
+			inline bool f_AppendArgs(t_CFirst const& _First, Args const& ... _Args);
+
+		};
+
+		class CMessageReader
+		{
+		private:
+			CDBusLibrary& mp_Lib;
+			DBusMessageIter mp_Iter;
+
+		public:
+			CMessageReader(CMessage const& _Message);
+			~CMessageReader();
+
+			template<typename t_CType>
+			bool f_PopArg(t_CType& _oArg);
+
+			inline bool f_PopArgs();
+
+			template<typename t_CFirst, typename... Args>
+			inline bool f_PopArgs(t_CFirst& _First, Args&... _Args);
+
+			bool f_ArgAvailable();
+		};
+
+		class CError
+		{
+		private:
+			CDBusLibrary& mp_Lib;
+			DBusError mp_Error;
+
+			friend CConnection;
+
+			CError(CDBusLibrary& _Lib);
+
+		public:
+			CError(CSystem& _Sys);
+			CError(CError const& _ToCopy) = delete;
+			CError(CError&& _ToMove);
+			~CError();
+
+			CError& operator=(CError const& _ToCopy) = delete;
+			CError& operator=(CError&& _ToMove);
+
+			bool f_IsSet() const;
+			char const*f_GetName() const;
+			char const*f_GetMessage() const;
+
+			void f_Clear();
+
+			void f_Take(DBusError& _FromHere);
+		};
+
+		static int const gc_DefaultTimeout = -1;
+
+		enum EDBusBus
+		{
+				EDBusBus_Session	// User specific bus
+			,	EDBusBus_System		// Machine specific bus
+		};
+
+		class CConnection
+		{
+		private:
+			enum EFlag
+			{
+					EFlag_None		= 0
+				,	EFlag_Private	= DMibBit(0)
+			};
+		private:
+			CDBusLibrary& mp_Lib;
+			DBusConnection* mp_pConnection;
+			EFlag mp_Flags;
+			CError mp_LastError;
+
+			friend CSystem;
+
+		public:
+
+			CConnection(CSystem& _Sys);
+			~CConnection();
+
+			CError const& f_GetLastError() const;
+
+			bool f_Open(char const* _pAddress, bool _bPrivate = false); // You will probably not use this, and almost never with _bPrivate == true.
+			bool f_Open(EDBusBus _Bus);
+			bool f_Close();
+
+			bool f_BlockingSendWithReply(CMessage& _Message, CMessage& _oReply, int _TimeoutMillis = gc_DefaultTimeout);
+
+		};
+
+		class CSystem
+		{
+		private:
+			NThread::CMutual mp_LibLock;
+			NPtr::TCUniquePointer<CDBusLibrary> mp_pLib;
+
+			friend CMessage;
+			friend CError;
+			friend CConnection;
+
+		public:
+			CSystem();
+			~CSystem();
+
+			bool f_ReadyForUse();	// f_IsOK will never return true until this has been called and returned true.
+
+			bool f_IsOK();
+
+		};
+
+
+		// 
+		// CMessageWriter Implementation
+		//
+
+		template<typename t_CType>
+		bool CMessageWriter::f_AppendArg(t_CType const& _Arg)
+		{
+			static_assert(NTraits::TCIsSame<t_CType, void>::mc_Value, "This type is not supported");
+			return false;
+		}
+		
+		template<>
+		bool CMessageWriter::f_AppendArg<uint8>(uint8 const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<int16>(int16 const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<uint16>(uint16 const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<int32>(int32 const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<uint32>(uint32 const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<int64>(int64 const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<uint64>(uint64 const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<bool>(bool const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<char const*>(char const* const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<NStr::CStr>(NStr::CStr const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<NStr::CStrSecure>(NStr::CStrSecure const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<NContainer::TCVector<uint8>>(NContainer::TCVector<uint8> const& _Value);
+
+		template<>
+		bool CMessageWriter::f_AppendArg<NContainer::TCVector<NStr::CStr>>(NContainer::TCVector<NStr::CStr> const& _Value);
+
+		bool CMessageWriter::f_AppendArgs()
+		{
+			return true;
+		}
+
+		template<typename t_CFirst, typename... Args>
+		bool CMessageWriter::f_AppendArgs(t_CFirst const& _First, Args const& ... _Args)
+		{
+			return 		f_AppendArg<t_CFirst>(_First)
+					&&	f_AppendArgs(_Args...);
+		}
+
+		// 
+		// CMessageReader Implementation
+		//
+
+		template<typename t_CType>
+		bool CMessageReader::f_PopArg(t_CType& _oArg)
+		{
+			static_assert(NTraits::TCIsSame<t_CType, void>::mc_Value, "This type is not supported");
+			return false;
+		}
+		
+		template<>
+		bool CMessageReader::f_PopArg<uint8>(uint8& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<int16>(int16& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<uint16>(uint16& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<int32>(int32& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<uint32>(uint32& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<int64>(int64& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<uint64>(uint64& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<NStr::CStr>(NStr::CStr& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<NStr::CStrSecure>(NStr::CStrSecure& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<bool>(bool& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<NContainer::TCVector<uint8>>(NContainer::TCVector<uint8>& _oValue);
+
+		template<>
+		bool CMessageReader::f_PopArg<NContainer::TCVector<NStr::CStr>>(NContainer::TCVector<NStr::CStr>& _oValue);
+
+		bool CMessageReader::f_PopArgs()
+		{
+			return true;
+		}
+
+		template<typename t_CFirst, typename... Args>
+		bool CMessageReader::f_PopArgs(t_CFirst& _oFirst, Args&... _oArgs)
+		{
+			return 		f_PopArg(_oFirst)
+					&&	f_PopArgs(_oArgs...);
+		}
+
+	} // Namespace DBus
+
+} // Namespace NMib
+
---
+++ Test/Test_Malterlib_Desktop_DBus.cpp
@@ -0,0 +1,341 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/Core/Core>
+
+#include <Mib/Desktop/DBus>
+
+using namespace NMib;
+
+namespace
+{
+
+	class CDBus_Tests : public NMib::NTest::CTest
+	{
+	public:
+
+		void f_DoTests()
+		{
+			NDBus::CSystem DBus;
+			DBus.f_ReadyForUse();
+			
+			if (!DBus.f_IsOK())
+				return; // Not supported on this system
+
+			DMibTestSuite("System")
+			{
+				DMibTest(DMibExpr(DBus.f_IsOK()) == DMibExpr(true))(ETest_FailAndStop);
+			};
+
+			DMibTestSuite("Message_Method")
+			{
+				NDBus::CMessage Msg(NDBus::EMessageType_Method, "com.hansoft.service", "/modules/test", "com.hansoft.test", "testMethod", DBus);
+			};
+
+			DMibTestSuite("Message_MethodReturn")
+			{
+				NDBus::CMessage Msg1(NDBus::EMessageType_Method, "com.hansoft.service", "/modules/test", "com.hansoft.test", "testMethod", DBus);
+				Msg1.f_SetSerial(100);
+				NDBus::CMessage Msg2(NDBus::EMessageType_MethodReturn, Msg1, DBus);
+			};
+
+			DMibTestSuite("Message_Signal")
+			{
+				NDBus::CMessage Msg(NDBus::EMessageType_Signal, "/modules/test", "com.hansoft.test", "testSignal", DBus);
+			};
+
+			DMibTestSuite("Message_Error")
+			{
+				NDBus::CMessage Msg1(NDBus::EMessageType_Method, "com.hansoft.service", "/modules/test", "com.hansoft.test", "testMethod", DBus);
+				Msg1.f_SetSerial(100);
+				NDBus::CMessage Msg2(NDBus::EMessageType_Error, Msg1, "com.hansoft.error", "TestErrorMessage", DBus);
+			};
+
+			DMibTestSuite("MessageWriter_AppendArg")
+			{
+				NDBus::CMessage Msg(NDBus::EMessageType_Method, "com.hansoft.service", "/modules/test", "com.hansoft.test", "testMethod", DBus);
+
+				NDBus::CMessageWriter Writer(Msg);
+
+				NContainer::TCVector<uint8> lBytes;
+				lBytes.f_SetLen(32);
+				NMem::fg_MemClear(lBytes.f_GetArray(), 32);
+
+				NContainer::TCVector<NStr::CStr> lStrings;
+				lStrings.f_Insert("One");
+				lStrings.f_Insert("Two");
+
+				NStr::CStr StringVar("X");
+
+				DMibTest(DMibExpr(Writer.f_AppendArg((uint8)0)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg((int16)1)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg((uint16)2)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg((int32)3)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg((uint32)4)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg((int64)5)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg((uint64)6)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg(StringVar)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg(false)) == DMibExpr(true))(ETest_FailAndStop);				
+				DMibTest(DMibExpr(Writer.f_AppendArg(lBytes)) == DMibExpr(true))(ETest_FailAndStop);
+				DMibTest(DMibExpr(Writer.f_AppendArg(lStrings)) == DMibExpr(true))(ETest_FailAndStop);
+			};
+
+			DMibTestSuite("MessageWriter_AppendArgs")
+			{
+				NDBus::CMessage Msg(NDBus::EMessageType_Method, "com.hansoft.service", "/modules/test", "com.hansoft.test", "testMethod", DBus);
+
+				NDBus::CMessageWriter Writer(Msg);
+
+				NContainer::TCVector<uint8> lBytes;
+				lBytes.f_SetLen(32);
+				NMem::fg_MemClear(lBytes.f_GetArray(), 32);
+
+				NContainer::TCVector<NStr::CStr> lStrings;
+				lStrings.f_Insert("One");
+				lStrings.f_Insert("Two");
+
+				bool bRet = Writer.f_AppendArgs(
+										(uint8)0
+									,	(int16)1
+									,	(uint16)2
+									, 	(int32)3
+									,	(uint32)4
+									,	(int64)5
+									,	(uint64)6
+									,	NStr::CStr("X")
+									,	false
+									, 	lBytes
+									,	lStrings);
+
+				DMibTest(DMibExpr(bRet) == DMibExpr(true));
+			};
+
+			DMibTestSuite("MessageReader_PopArg")
+			{
+				NDBus::CMessage Msg(NDBus::EMessageType_Method, "com.hansoft.service", "/modules/test", "com.hansoft.test", "testMethod", DBus);
+
+				{
+					NDBus::CMessageWriter Writer(Msg);
+
+					NContainer::TCVector<uint8> lBytes;
+					lBytes.f_SetLen(32);
+					NMem::fg_MemClear(lBytes.f_GetArray(), 32);
+
+					NContainer::TCVector<NStr::CStr> lStrings;
+					lStrings.f_Insert("One");
+					lStrings.f_Insert("Two");
+
+					bool bRet = Writer.f_AppendArgs(
+											(uint8)0
+										,	(int16)1
+										,	(uint16)2
+										, 	(int32)3
+										,	(uint32)4
+										,	(int64)5
+										,	(uint64)6
+										,	NStr::CStr("X")
+										,	false
+										, 	lBytes
+										,	lStrings);
+
+					DMibTest(DMibExpr(bRet) == DMibExpr(true));
+				}
+
+				{
+					NDBus::CMessageReader Reader(Msg);
+
+					DMibTest(DMibExpr(Reader.f_ArgAvailable()) == DMibExpr(true));
+
+					uint8 UInt8Var = 100;
+					int16 Int16Var = 100;
+					uint16 UInt16Var = 100;
+					int32 Int32Var = 100;
+					uint32 UInt32Var = 100;
+					int64 Int64Var = 100;
+					uint64 UInt64Var = 100;
+					NStr::CStr StringVar = "Y";
+					bool bBoolVar = true;
+					NContainer::TCVector<uint8> lBytesVar;
+					NContainer::TCVector<NStr::CStr> lStringsVar;
+
+					DMibTest(DMibExpr(Reader.f_PopArg(UInt8Var)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(Int16Var)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(UInt16Var)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(Int32Var)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(UInt32Var)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(Int64Var)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(UInt64Var)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(StringVar)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(bBoolVar)) == DMibExpr(true))(ETest_FailAndStop);				
+					DMibTest(DMibExpr(Reader.f_PopArg(lBytesVar)) == DMibExpr(true))(ETest_FailAndStop);
+					DMibTest(DMibExpr(Reader.f_PopArg(lStringsVar)) == DMibExpr(true))(ETest_FailAndStop);
+
+					DMibTest(DMibExpr(UInt8Var) == DMibExpr(0));
+					DMibTest(DMibExpr(Int16Var) == DMibExpr(1));
+					DMibTest(DMibExpr(UInt16Var) == DMibExpr(2));
+					DMibTest(DMibExpr(Int32Var) == DMibExpr(3));
+					DMibTest(DMibExpr(UInt32Var) == DMibExpr(4));
+					DMibTest(DMibExpr(Int64Var) == DMibExpr(5));
+					DMibTest(DMibExpr(UInt64Var) == DMibExpr(6));
+					DMibTest(DMibExpr(StringVar) == DMibExpr(NStr::CStr("X")));
+					DMibTest(DMibExpr(bBoolVar) == DMibExpr(false));
+					{
+						NContainer::TCVector<uint8> lExpectedBytes;
+						lExpectedBytes.f_SetLen(32);
+						NMem::fg_MemClear(lExpectedBytes.f_GetArray(), 32);
+						DMibTest(DMibExpr(lBytesVar) == DMibExpr(lExpectedBytes));
+					}
+					{
+						NContainer::TCVector<NStr::CStr> lExpectedStrings;
+						lExpectedStrings.f_Insert("One");
+						lExpectedStrings.f_Insert("Two");						
+						DMibTest(DMibExpr(lStringsVar) == DMibExpr(lExpectedStrings));
+					}
+					
+					DMibTest(DMibExpr(Reader.f_ArgAvailable()) == DMibExpr(false));
+
+					bool bRet3 = Reader.f_PopArgs( UInt32Var );
+
+					DMibTest(DMibExpr(bRet3) == DMibExpr(false));
+				}
+			};
+
+			DMibTestSuite("MessageReader_PopArgs")
+			{
+				NDBus::CMessage Msg(NDBus::EMessageType_Method, "com.hansoft.service", "/modules/test", "com.hansoft.test", "testMethod", DBus);
+
+				{
+					NDBus::CMessageWriter Writer(Msg);
+
+					NContainer::TCVector<uint8> lBytes;
+					lBytes.f_SetLen(32);
+					NMem::fg_MemClear(lBytes.f_GetArray(), 32);
+
+					NContainer::TCVector<NStr::CStr> lStrings;
+					lStrings.f_Insert("One");
+					lStrings.f_Insert("Two");
+
+					bool bRet = Writer.f_AppendArgs(
+											(uint8)0
+										,	(int16)1
+										,	(uint16)2
+										, 	(int32)3
+										,	(uint32)4
+										,	(int64)5
+										,	(uint64)6
+										,	NStr::CStr("X")
+										,	false
+										, 	lBytes
+										,	lStrings);
+
+					DMibTest(DMibExpr(bRet) == DMibExpr(true));
+				}
+
+				{
+					NDBus::CMessageReader Reader(Msg);
+
+					DMibTest(DMibExpr(Reader.f_ArgAvailable()) == DMibExpr(true));
+
+					uint8 UInt8Var = 100;
+					int16 Int16Var = 100;
+					uint16 UInt16Var = 100;
+					int32 Int32Var = 100;
+					uint32 UInt32Var = 100;
+					int64 Int64Var = 100;
+					uint64 UInt64Var = 100;
+					NStr::CStr StringVar = "Y";
+					bool bBoolVar = true;
+					NContainer::TCVector<uint8> lBytesVar;
+					NContainer::TCVector<NStr::CStr> lStringsVar;
+
+					bool bRet2 = Reader.f_PopArgs(
+												UInt8Var
+											,	Int16Var
+											,	UInt16Var
+											,	Int32Var
+											,	UInt32Var
+											,	Int64Var
+											,	UInt64Var
+											,	StringVar
+											,	bBoolVar
+											,	lBytesVar
+											,	lStringsVar );
+
+					DMibTest(DMibExpr(bRet2) == DMibExpr(true)) (ETest_FailAndStop);
+					DMibTest(DMibExpr(UInt8Var) == DMibExpr(0));
+					DMibTest(DMibExpr(Int16Var) == DMibExpr(1));
+					DMibTest(DMibExpr(UInt16Var) == DMibExpr(2));
+					DMibTest(DMibExpr(Int32Var) == DMibExpr(3));
+					DMibTest(DMibExpr(UInt32Var) == DMibExpr(4));
+					DMibTest(DMibExpr(Int64Var) == DMibExpr(5));
+					DMibTest(DMibExpr(UInt64Var) == DMibExpr(6));
+					DMibTest(DMibExpr(StringVar) == DMibExpr(NStr::CStr("X")));
+					DMibTest(DMibExpr(bBoolVar) == DMibExpr(false));
+					{
+						NContainer::TCVector<uint8> lExpectedBytes;
+						lExpectedBytes.f_SetLen(32);
+						NMem::fg_MemClear(lExpectedBytes.f_GetArray(), 32);
+						DMibTest(DMibExpr(lBytesVar) == DMibExpr(lExpectedBytes));
+					}
+
+					{
+						NContainer::TCVector<NStr::CStr> lExpectedStrings;
+						lExpectedStrings.f_Insert("One");
+						lExpectedStrings.f_Insert("Two");						
+						DMibTest(DMibExpr(lStringsVar) == DMibExpr(lExpectedStrings));
+					}
+
+					DMibTest(DMibExpr(Reader.f_ArgAvailable()) == DMibExpr(false));
+
+					bool bRet3 = Reader.f_PopArgs( UInt32Var );
+
+					DMibTest(DMibExpr(bRet3) == DMibExpr(false));
+				}
+			};
+
+			DMibTestSuite("Connection")
+			{
+				NDBus::CMessage Msg(	NDBus::EMessageType_Method
+									,	"org.freedesktop.DBus"
+									,	"/"
+									,	"org.freedesktop.DBus"
+									, 	"GetId"
+									,	DBus);
+
+				NDBus::CConnection Connection(DBus);
+
+				bool bOpened = Connection.f_Open(NDBus::EDBusBus_Session);
+
+				DMibTest(DMibExpr(bOpened) == DMibExpr(true)) (ETest_FailAndStop);
+
+				NDBus::CMessage Reply(DBus);
+
+				bool bSentAndReceived = Connection.f_BlockingSendWithReply(Msg, Reply);
+				
+				DMibTest(DMibExpr(bSentAndReceived) == DMibExpr(true)) (ETest_FailAndStop);
+
+				
+				{
+					NDBus::CMessageReader Reader(Reply);
+
+					DMibTest(DMibExpr(Reader.f_ArgAvailable()) == DMibExpr(true));
+
+					NStr::CStr IDString;
+
+					Reader.f_PopArgs(IDString);
+
+					DMibTest(DMibExpr(IDString.f_IsEmpty()) == DMibExpr(false));
+				}
+
+				bool bClosed = Connection.f_Close();
+				
+				DMibTest(DMibExpr(bClosed) == DMibExpr(true));
+
+			};
+
+		}
+			
+	};
+
+	DMibTestRegister(CDBus_Tests, Malterlib::Desktop);
+}
